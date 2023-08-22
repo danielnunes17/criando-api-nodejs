dd45a85cabd5e767b80b1662fa8f7774911df27e
Daniel Nunes <79380881+danielnunes17@users.noreply.github.com>
2023-08-22 15:46:37 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo ao Node Store';
 module.exports = {
   conectString: 'sua string de conecção',
   sendgridKey: 'TSG.AUBMCFcrQNqWlfSuIfMs2Q.RuEIcv1R8aM3N_zqWyYajGWcpy28kPU15e8MG9cCgRg',
-  containerConnectionString: 'sua estring de conecção com container'
+  containerConnectionString: 'sua string de conecção do container'
 }
